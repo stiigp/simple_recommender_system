defbc6bdc4d3632defb7196f486bb9aea891cdf3
stiigp <lucaspanuccidev@gmail.com>
2025-03-09 00:07:45 -0300

    Criando o endpoint movies por id

--- api/main.py
+++ api/main.py
@@ -1,15 +1,44 @@
 from fastapi import FastAPI
 import requests
 from config import SEARCHLY_URL
+from elasticsearch import Elasticsearch
 
 app = FastAPI()
+es = Elasticsearch(SEARCHLY_URL)
+
+MOVIES_INDEX_NAME = "movies_index"
 
 @app.get("/")
 async def root():
     return {"message": "eita porra dum caralho agora fudeu de vez"}
 
-@app.get("/movies/")
+
+@app.get("/movies_list/{start}/{movies_number}")
 async def getMovies(start:int=0, movies_number:int=10):
-    url = SEARCHLY_URL + f"/movies_index/_search?pretty=true&q=*:*&from={start}&size={movies_number}"
-    response = requests.get(url=url)
-    return response.json()
+
+    req_body = {
+        "query": {
+            "match_all": {}
+        },
+        "size": movies_number,
+        "from": start
+    }
+
+    response = es.search(index=MOVIES_INDEX_NAME, body=req_body)
+
+    return [film['_source'] for film in response['hits']['hits']]
+
+@app.get('/movies/{movieId}')
+async def getMovieById(movieId:int):
+    body = {
+        'query': {
+            "term": {
+                "body.movieId": {
+                    "value": movieId
+                }
+            }
+        }
+    }
+
+    response = es.search(index=MOVIES_INDEX_NAME, body=body)
+    return response['hits']['hits']
